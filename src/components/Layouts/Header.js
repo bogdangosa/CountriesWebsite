0173a6c51d4c595810f2d08221abf0bf761519bc
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2022-01-29 23:23:00 +0200

    added dark mode

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,12 +1,42 @@
-import react from "react";
+import react, { useState,useEffect } from "react";
 import styles from '../../../styles/Header.module.css';
 
 
 const Header = () =>{
+    const [darkTheme, setDarkTheme] = useState(undefined);
+
+    const handleToggle = (event) => {
+      setDarkTheme(event.target.checked);
+    };
+
+  
+    useEffect(() => {
+      if (darkTheme !== undefined) {
+        if (darkTheme) {
+          // Set value of  darkmode to dark
+          document.documentElement.setAttribute('data-theme', 'dark');
+          window.localStorage.setItem('theme', 'dark');
+        } else {
+          // Set value of  darkmode to light
+          document.documentElement.removeAttribute('data-theme');
+          window.localStorage.setItem('theme', 'light');
+        }
+      }
+    }, [darkTheme]);
+  
+    useEffect(() => {
+      const root = window.document.documentElement;
+      const initialColorValue = root.style.getPropertyValue(
+        '--initial-color-mode'
+      );
+      // Set initial darkmode to light
+      setDarkTheme(initialColorValue === 'dark');
+    }, []);
+
     return(
         <div className={styles.Header}>
             <h1 className={styles.Header_title}>Where in the World?</h1>
-            <p>Dark Mode</p>
+            <p onClick={()=>setDarkTheme(!darkTheme)} className={styles.DarkModeToggle}>{darkTheme?"Dark Mode":"Light Mode"}</p>
         </div>
     )
 }
